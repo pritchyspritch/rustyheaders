81f00e916ff24a24357abfce6184433dca143589
pritchyspritch <47423802+pritchyspritch@users.noreply.github.com>
2024-11-06 13:07:46 +0000

    Test x-frame-options

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 
 use clap::Parser;
-use reqwest::Response;
+use reqwest::{header::HeaderMap, Response};
 
 #[tokio::main]
 async fn get_request(url: String) -> Result<Response, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
-    let headers = client
+    let response = client
         .get(url)
         .send()
         .await?;
     
-    Ok(headers)
+    Ok(response)
+}
+
+fn assess_headers(response_headers: &HeaderMap) {
+    if response_headers.get("x-frame-options").unwrap() == &"SAMEORIGIN" {
+        println!("SAMEORIGIN set for X-FRAME-OPTIONS header, this header is deprecated and can be kept for compatibility purposes for old browsers but should not be relied upon.\nUse 'frame-ancestors' and 'content-security-policy' headers instead. See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options.")
+    } else if response_headers.get("x-frame-options").is_none() {
+        println!("No X-FRAME-OPTIONS header found, check for 'frame-ancestors' and 'content-security-policy' headers.")
+    }
 }
 
 #[derive(Parser)]
@@ -22,8 +30,10 @@ fn main() {
     let args = Args::parse();
     // let headers = get_request(args.url).unwrap();
     match get_request(args.url) {
-        Ok(headers) => {
-            println!("Headers:\n{:#?}", headers.headers());
+        Ok(response) => {
+            let response_headers = response.headers();
+            println!("Headers:\n{:#?}", response_headers);
+            assess_headers(response_headers);
         },
         Err(e) => {
             println!("Error calling url: {}", e)
